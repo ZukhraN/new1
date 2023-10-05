2e1039168259509974ea869031a9bc7e06e56f4d
ZukhraN <zukhra.nurmanova@gmail.com>
2023-10-05 09:25:37 +0600

    !!! added in 14 string before text

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -11,6 +11,6 @@ def test_find_in_the_browser(open_browser):
 def test_new_find(open_browser):
 
     browser.open('https://google.com')
-    browser.element('[name="q"]').should(be.blank).type('bcbvjcjnxjnjnxnkcm!!!').press_enter()
+    browser.element('[name="q"]').should(be.blank).type('!!!bcbvjcjnxjnjnxnkcm').press_enter()
     browser.element('#extabar #result-stats').should(have.text('Результатов: примерно 0'))
 
